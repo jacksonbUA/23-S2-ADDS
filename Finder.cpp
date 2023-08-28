0d1a776e64e3c33e7f09da7bdca33ef1c1559327
jacksonbUA <jackson.bunting@student.adelaide.edu.au>
2023-08-28 15:29:58 +0930

    refactor, testing intial performance with little optimisation

--- Finder.cpp
+++ Finder.cpp
@@ -2,20 +2,23 @@
 using namespace std;
 
 vector<int> Finder::findSubstrings(string s1, string s2) {
-  vector<int> result{0};
-  int result_index = 0;
-  for(size_t i = 1; i <= s2.size(); i++) {
-    size_t found = s1.substr(result[result_index],s1.length()).find(s2.substr(0, result_index));
+  vector<int> result;
+  int s1_length = s1.length();
+  int s2_index = 0;
+  int s2_remaining = s2.length();
+  for (size_t i = 0; i < s1_length ; i++) {
+
+    size_t found = s1.find(s2.substr(0, s2_index));
+
     if (found != string::npos) {
-      result_index++;
       result.push_back(found);
-    } 
+      s2_remaining--;
+      s2_index++;
+    }
+
     else {
-      for(; i <= s2.size(); i++) {
-        result.push_back(-1);
-      }
+      result.insert(result.end(), s2_remaining, -1);
+      return result;
     }
   }
-  result.erase(result.begin());
-  return result;
 }
