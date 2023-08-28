82bb57e63101dc7a0de86778956611208a39d465
jacksonbUA <jackson.bunting@student.adelaide.edu.au>
2023-08-28 15:08:11 +0930

    fixes substring optimisation by including result index

--- Finder.cpp
+++ Finder.cpp
@@ -5,7 +5,7 @@ vector<int> Finder::findSubstrings(string s1, string s2) {
   vector<int> result{0};
   int result_index = 0;
   for(size_t i = 1; i <= s2.size(); i++) {
-    size_t found = s1.substr(result[result_index],s1.length()).find(s2.substr(0, i));
+    size_t found = s1.substr(result[result_index],s1.length()).find(s2.substr(0, result_index));
     if (found != string::npos) {
       result_index++;
       result.push_back(found);
