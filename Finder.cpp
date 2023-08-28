b4be280efea35bc4a2aa5571c4d94668d66af38d
jacksonbUA <jackson.bunting@student.adelaide.edu.au>
2023-08-28 16:16:41 +0930

    fixed 0 indexing

--- Finder.cpp
+++ Finder.cpp
@@ -8,6 +8,10 @@ vector<int> Finder::findSubstrings(string s1, string s2) {
   int s2_remaining = s2.length();
   int s1_last_pos = 0;
   for (size_t i = 0; i < s1_length ; i++) {
+    if (s2_remaining <= 0) {
+      result.erase(result.begin());
+      return result;
+    }
 
     //size_t found = s1.substr(result[s1_last_pos], s1_length).find(s2.substr(0, s2_index));
       size_t found = s1.find(s2.substr(0, s2_index), s1_last_pos);
