082e0437e2cbb8a91bba54871e4ad1274a0fcb06
jacksonbUA <jackson.bunting@student.adelaide.edu.au>
2023-08-28 14:40:20 +0930

    attempts to fix invalid int

--- Finder.cpp
+++ Finder.cpp
@@ -5,7 +5,7 @@ vector<int> Finder::findSubstrings(string s1, string s2) {
   vector<int> result;
   int result_index = 0;
   for(size_t i = 1; i <= s2.size(); i++) {
-    size_t found = s1.find(s2.substr(result[result_index] - '0', i));
+    size_t found = s1.find(s2.substr(result[result_index], i));
     if (found != string::npos) {
       result_index++;
       result.push_back(found);
