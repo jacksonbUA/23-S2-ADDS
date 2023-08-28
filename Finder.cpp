e275fc950c3f7e9b3cd77ad7f0c447c917f9daf6
jacksonbUA <jackson.bunting@student.adelaide.edu.au>
2023-08-28 16:31:45 +0930

    trying to itterate of s2 instead

--- Finder.cpp
+++ Finder.cpp
@@ -1,7 +1,9 @@
+/*
 #include "Finder.h"
 using namespace std;
 
 vector<int> Finder::findSubstrings(string s1, string s2) {
+  
   vector<int> result {0};
   size_t s1_length = s1.length();
   int s2_index = 1;
@@ -13,8 +15,7 @@ vector<int> Finder::findSubstrings(string s1, string s2) {
       return result;
     }
 
-    //size_t found = s1.substr(result[s1_last_pos], s1_length).find(s2.substr(0, s2_index));
-      size_t found = s1.find(s2.substr(0, s2_index), s1_last_pos);
+    size_t found = s1.find(s2.substr(0, s2_index), s1_last_pos);
     if (found != string::npos) {
       result.push_back(found);
       s2_remaining--;
@@ -31,3 +32,43 @@ vector<int> Finder::findSubstrings(string s1, string s2) {
   result.erase(result.begin());
   return result;
 }
+
+
+
+*/
+
+#include "Finder.h"
+using namespace std;
+
+vector<int> Finder::findSubstrings(string s1, string s2) {
+  
+  vector<int> result {0};
+  size_t s1_length = s1.length();
+  int s2_index = 1;
+  int s2_remaining = s2.length();
+  int s1_last_pos = 0;
+  for (size_t i = 0; i < s2.length(); i++) {
+    if (s2_remaining <= 0) {
+      result.erase(result.begin());
+      return result;
+    }
+
+    size_t found = s2.substr(0, s2_index).find(s1, s1_last_pos);
+    if (found != string::npos) {
+      result.push_back(found);
+      s2_remaining--;
+      s2_index++;
+      s1_last_pos = i;
+    }
+
+    else {
+      result.insert(result.end(), s2_remaining, -1);
+      result.erase(result.begin());
+      return result;
+    }
+  }
+  result.erase(result.begin());
+  return result;
+}
+
+
