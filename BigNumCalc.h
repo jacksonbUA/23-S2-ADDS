26b1ff996b1ac07e0629f72c9427c9a0f561f507
TalkingF <140305263+TalkingF@users.noreply.github.com>
2023-09-11 22:23:31 +0930

    Update BigNumCalc.h

--- BigNumCalc.h
+++ BigNumCalc.h
@@ -9,7 +9,7 @@ class BigNumCalc {
   private:
 
   public:
-  bigNumCalc();
+  BigNumCalc();
   std::list<int> buildBigNum(std::string numString);
   std::list<int> add(std::list<int> num1, std::list<int> num2);
   std::list<int> sub(std::list<int> num1, std::list<int> num2);
