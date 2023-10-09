1cf3db790facb8661393b516ee61fdaf9da71e11
TalkingF <140305263+TalkingF@users.noreply.github.com>
2023-10-09 17:04:43 +1030

    Update Autocomplete.h

--- Autocomplete.h
+++ Autocomplete.h
@@ -6,12 +6,12 @@
 #include <string>
 
 
-class AutoComplete {
+class Autocomplete {
    
 
     public:
     TrieNode* root;
-    AutoComplete();
+    Autocomplete();
     std::vector<std::string> getSuggestions(std::string partialWord);  // return the known words that start with partialWord
     void insert(std::string word); // add a word to the known words
 
